abd50391bda3b14b6b42b20472c7195863aa0946
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-10-26 15:01:08 +0300

    transport sql queries completed

--- internal/repository/transport.go
+++ internal/repository/transport.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	"vtb_api/internal/entities"
 )
@@ -14,17 +15,32 @@ func NewTransport(dp *sqlx.DB) Transport {
 }
 
 func (t *transport) GetById(id int) (*entities.Transport, error) {
-	return nil, nil
+	var transp entities.Transport
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", transportTable)
+	err := t.db.Get(&transp, query, id)
+	return &transp, err
 }
 
 func (t *transport) AddTransport(tr *entities.Transport) (int, error) {
-	return 0, nil
+	var id int
+	query := fmt.Sprintf("INSERT INTO %s "+
+		"(can_be_rented, transport_type, model, color, identifier, description, latitude, longitude, minute_price, day_price) "+
+		"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, 10)  RETURNING id", transportTable)
+	row := t.db.QueryRow(query, tr.CanBeRented, tr.TransportType, tr.Model, tr.Color, tr.Identifier, tr.Description, tr.Latitude, tr.Longitude, tr.MinutePrice, tr.DayPrice)
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
 }
 
 func (t *transport) Update(id int, tr *entities.Transport) error {
-	return nil
+	query := fmt.Sprintf("UPDATE %s SET can_be_rented=$1, model=$2, color=$3, identifier=$4, description=$5, latitude=$6, longitude=$7, minute_price=$8, day_price=%9 ", transportTable)
+	_, err := t.db.Exec(query, tr.CanBeRented, tr.Model, tr.Color, tr.Identifier, tr.Description, tr.Latitude, tr.Longitude, tr.MinutePrice, tr.DayPrice)
+	return err
 }
 
 func (t *transport) Delete(id int) error {
-	return nil
+	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1", transportTable)
+	_, err := t.db.Exec(query, id)
+	return err
 }
